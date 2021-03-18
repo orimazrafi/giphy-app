d92af1a7a7f83bf630dd6d7bb459772d0e1d86e9
ori.mazrafi <ori.mazrafi@imperva.com>
2021-03-18 17:52:44 +0200

    /#bootstrap added bootstrap and style login page.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,38 +1,67 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 const Login = () => {
+  const history = useHistory();
   const [credentials, setCredentials] = useState({
     username: "",
     password: "",
   });
+
+  const submit = () => {
+    history.push("/search");
+  };
   return (
-    <div>
-      <input
-        name="username"
-        type="text"
-        value={credentials.username}
-        onChange={(e) =>
-          setCredentials((pre) => ({ ...pre, [e.target.name]: e.target.value }))
-        }
-      />
-      <input
-        name="password"
-        type="password"
-        value={credentials.password}
-        onChange={(e) =>
-          setCredentials((pre) => ({ ...pre, [e.target.name]: e.target.value }))
-        }
-      />
-      <button
-        onClick={() =>
-          alert(
-            `username is ${credentials.username} password is ${credentials.password}`
-          )
-        }
-      >
-        Submit
-      </button>
-    </div>
+    <Container fluid>
+      <Form>
+        <Row>
+          <Col xs={4}>
+            <Form.Group controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                name="username"
+                type="text"
+                value={credentials.username}
+                onChange={(e) =>
+                  setCredentials((pre) => ({
+                    ...pre,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
+                placeholder="Enter username..."
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={4}>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                type="password"
+                value={credentials.password}
+                onChange={(e) =>
+                  setCredentials((pre) => ({
+                    ...pre,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
+                placeholder="Enter Password..."
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="primary" type="submit" onClick={submit}>
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 };
 
