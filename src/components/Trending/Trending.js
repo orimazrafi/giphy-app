c8d860af08f40d8dd3688ed0191e012584a2370c
ori.mazrafi <ori.mazrafi@imperva.com>
2021-03-18 20:08:27 +0200

    /#giphy-api added the api with axios. only trending for now.

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Trending = () => {
-  return <div>Trending</div>;
+  const [gifs, setGifs] = useState([]);
+  useEffect(() => {
+    const getGif = async () => {
+      const trendingGifs = await axios.get(
+        "https://api.giphy.com/v1/gifs/trending?api_key=PuUb7zLUSCj82KmilzLEYqVfMU7lHs39"
+      );
+      setGifs(trendingGifs.data.data);
+    };
+
+    getGif();
+  }, []);
+  console.log({ gifs });
+
+  return (
+    <>
+      <div>Trending</div>
+      <div>Please filter out:</div>
+      <div style={{ float: "left" }}>
+        {gifs.map((gif) => (
+          <img
+            key={Math.random()}
+            src={`${gif.images.fixed_height_downsampled.url}`}
+            alt="this slowpoke moves"
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Trending;
