20c375bd1cf12c7dcb94cda85a8b5cfb583aea57
ori.mazrafi <ori.mazrafi@imperva.com>
2021-03-18 17:01:55 +0200

    /#routes added all possible routes.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Login from "./components/Login/Login";
 // import "./App.css";
-import HomePage from "./components/HomePage/HomePage";
+import Search from "./components/Search/Search";
+import NotFound from "./components/NotFound/NotFound";
+import Favourites from "./components/Favourites/Favourites";
+import Trending from "./components/Trending/Trending";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/login">
-          <Login />
+        <Route exact path="/">
+          <Redirect to="/login" />
         </Route>
-        <Route path="/home-page">
-          <HomePage />
-        </Route>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/favourites" component={Favourites} />
+        <Route exact path="/tranding" component={Trending} />
+        <Route path="*" exact component={NotFound} />
       </Switch>
     </Router>
   );
