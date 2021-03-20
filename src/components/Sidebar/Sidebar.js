fa80bb1ec7a1a2324b0f1753d886950445e1ff63
ori.mazrafi <ori.mazrafi@imperva.com>
2021-03-20 18:23:32 +0200

    /#side-bar styled sidebar links.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link, Route, Redirect } from "react-router-dom";
 import Switch from "react-bootstrap/esm/Switch";
@@ -6,24 +6,51 @@ import Trending from "../Trending/Trending";
 import Favourites from "../Favourites/Favourites";
 import Search from "../Search/Search";
 import ErrorBoundryComponent from "../ErrorBoundryComponent/ErrorBoundryComponent";
+import styled from "styled-components";
+import { SIDEBAR_ACTIVE } from "../../constants";
 
 const Sidebar = () => {
+  const [activeLink, setActiveLink] = useState(SIDEBAR_ACTIVE);
+  const handleClick = (title) => {
+    setActiveLink({
+      SIDEBAR_ACTIVE,
+      [title]: true,
+    });
+  };
+
   return (
     <Container fluid>
       <Row>
-        <Col xs={2}>
+        <ColWrapper xs={2}>
           <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li>
-              <Link to="/home-page/trending">Trending</Link>
+            <li onClick={() => handleClick("Trending")}>
+              <LinkWrapper to="/home-page/trending">
+                <h5
+                  className={activeLink["Trending"] === true ? " active" : ""}
+                >
+                  Trending
+                </h5>
+              </LinkWrapper>
             </li>
-            <li>
-              <Link to="/home-page/search">Search</Link>
+            <li onClick={() => handleClick("Search")}>
+              <LinkWrapper to="/home-page/search">
+                <h5 className={activeLink["Search"] === true ? " active" : ""}>
+                  Search
+                </h5>
+              </LinkWrapper>
             </li>
-            <li>
-              <Link to="/home-page/favourites">Favourites</Link>
+            <li onClick={() => handleClick("Favourites")}>
+              <LinkWrapper to="/home-page/favourites">
+                <h5
+                  className={activeLink["Favourites"] === true ? " active" : ""}
+                >
+                  Favourites
+                </h5>
+              </LinkWrapper>
             </li>
           </ul>
-        </Col>
+        </ColWrapper>
+
         <Col>
           <Switch>
             <Route exact path="/home-page">
@@ -40,3 +67,23 @@ const Sidebar = () => {
 };
 
 export default ErrorBoundryComponent(Sidebar);
+
+const ColWrapper = styled(Col)`
+  height: calc(100vh - 56px);
+  background: #eee;
+  max-width: 200px;
+  padding-top: 10px;
+`;
+
+const LinkWrapper = styled(Link)`
+  & > h5 {
+    margin-top: 20px;
+  }
+  &:hover {
+    text-decoration: none;
+    color: #5690bd;
+  }
+  h5.active {
+    color: #021829;
+  }
+`;
